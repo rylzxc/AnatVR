716ebbc7a3a03fe9a8841b8d9bd21411f66e3217
Daryl <chuachdaryl@gmail.com>
2023-05-11 16:28:59 +0800

    cleanup, add haptics, outline & audio

--- Assets/_Course Library/Scripts/Misc/Billboard.cs
+++ Assets/_Course Library/Scripts/Misc/Billboard.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Camera mainCamera;
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void LateUpdate()
     {
-        transform.LookAt(Camera.main.transform.position, Vector3.up);
+        transform.LookAt(mainCamera.transform);
+        transform.Rotate(0, 180, 0);
     }
 }
