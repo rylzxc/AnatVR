c383e8058e0df31d4a82ef52b24eb124a341c7c8
Daryl <chuachdaryl@gmail.com>
2023-05-19 16:47:03 +0800

    add auto-regen lighting
    scene goes dark after resetting, lets regenerate lighting to prevent this bug.
    
    selection of objects was initially done with grip button, lets change it to trigger button to make it more intuitive

---
+++ Assets/_Course Library/Scripts/Misc/ShiftObject.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShiftObject : MonoBehaviour
+{
+    public List<GameObject> objects = null;
+    private int listCount = 0;
+    private Vector3 previousPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objects = new List<GameObject>(GameObject.FindGameObjectsWithTag("Selectable"));
+        listCount = objects.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void BringObjectCloser() {
+        
+        if (ObjectTracker.objectTracker != null && objects.Count == listCount) {
+            if (ObjectTracker.objectTracker.activeInHierarchy) {
+                Vector3 targetPosition = new Vector3(2.00f, 0.30f, 1.30f);
+
+                previousPosition = ObjectTracker.objectTracker.transform.position;
+                ObjectTracker.objectTracker.transform.position = targetPosition;
+
+                objects.Remove(ObjectTracker.objectTracker);
+                
+                foreach (GameObject obj in objects) {
+                    obj.SetActive(false);
+                }
+            }
+        } else {
+            // Revert back to original position
+            foreach (GameObject obj in objects) {
+                obj.SetActive(true);
+            }
+
+            ObjectTracker.objectTracker.transform.position = previousPosition;
+
+            objects.Add(ObjectTracker.objectTracker);
+
+        }
+    }
+}
